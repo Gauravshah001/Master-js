633ac24f07df77d99272dfbc1e4b8e2efbc12b7c
Gauravshah001@example.com <Gauravshah001@example.com>
2025-06-24 12:56:45 -1100

    Part 3

--- Control Flow/dowhile.js
+++ Control Flow/dowhile.js
@@ -1,10 +1,52 @@
+//While loop
 
-for(let i=0;i<20;i++){
-    if(i==5){
-        console.log("5 is detected")
-    
-    }
-    
-    
-console.log(`${i}`)
+let i=0
+while(i<=10){
+    console.log(`value is ${i}`);
+    i=i+2;
+
 }
+
+//Output:
+// value is 0
+// value is 2
+// value is 4
+// value is 6
+// value is 8
+// value is 10
+
+
+let myarray=["batman","superman","antman"]
+let arr=0;
+while (arr <myarray.length) {
+    console.log(`the element is ${myarray[arr]}`);
+    arr=arr+1;
+
+}
+//Output :
+// the element is batman
+// the element is superman
+// the element is antman
+
+
+//Dowhile loop
+
+let score=1;
+do{
+    console.log(`score is ${score}`)
+    score++
+}
+while(score<=10);
+
+//Output
+
+// score is 1
+// score is 2
+// score is 3
+// score is 4
+// score is 5
+// score is 6
+// score is 7
+// score is 8
+// score is 9
+// score is 10
