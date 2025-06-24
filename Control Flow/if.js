14454e19add0479730d4a86c963d17df49365704
Gauravshah001@example.com <Gauravshah001@example.com>
2025-06-24 12:09:46 -1100

    Part 1

---
+++ Control Flow/if.js
@@ -0,0 +1,26 @@
+//If else
+
+const age=18;
+if(age>=18){
+    console.log("valid user");
+
+}
+else{
+    console.log("Invalid user");
+}
+
+//Switch statement
+
+const day="Monday"
+switch(day){
+    case "Monday":
+        console.log("Monday is frea");
+        break;
+
+    case "Tuesday":
+    console.log("tuesday night");
+    break;
+    default:
+        console.log("invalid")
+
+}
